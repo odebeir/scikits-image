f1dc7ebcd3402f4027eff731b2a559d40efd7f4e
Olivier Debeir <odebeir@ulb.ac.be>
2013-05-17 12:19:45 +0200

    working seams

--- skimage/feature/seams.py
+++ skimage/feature/seams.py
@@ -15,29 +15,49 @@ def compute_scores(img):
 
     Returns
     -------
-    imy : ndarray
+    cost : ndarray
         cost matrix
+    direction : ndarray
+        direction to the previous pixel (contains -1,0,1 for each pixel)
 
     """
 
     m,n = img.shape
 
+    img = img
+
     d = np.dstack((img[:,1:-1],img[:,0:-2],img[:,2:]))
-    best_id = np.argmax(d,axis=2)
-    best_id = np.hstack((np.zeros((m,1)),best_id,np.zeros((m,1))))
+    direction = np.argmax(d,axis=2)
+    direction = np.hstack((np.zeros((m,1)),direction,np.zeros((m,1))))
     scores = np.zeros_like(img,dtype=int)
     scores[0,:] = img[0,:]
     for i in range(1,m):
-        id0 = best_id[i-1,:]==0
-        id1 = best_id[i-1,:]==1
-        id2 = best_id[i-1,:]==2
+        id0 = direction[i-1,:]==0
+        id1 = direction[i-1,:]==1
+        id2 = direction[i-1,:]==2
 
         scores[i,id0] = scores[i-1,id0] + img[i,id0]
         scores[i,id1] = scores[i-1,np.roll(id1,-1)] + img[i,id1]
         scores[i,id2] = scores[i-1,np.roll(id2,+1)] + img[i,id2]
 
+    return (scores,direction)
 
-    #back track one single seam
+def backtrack(scores,best_id):
+    """backtrack from each pixel of the bottom line
+        returns path array
+
+    Parameters
+    ----------
+    image : ndarray
+        Input image.
+
+    Returns
+    -------
+    path : ndarray
+        best path for each of the bottom pixel up to the top
+
+    """
+    m,n = scores.shape
     path = np.zeros((m,n))
     path[-1,:] = np.arange(n)
     for i in range(m-2,-1,-1):
@@ -49,48 +69,56 @@ def compute_scores(img):
         path[i,id1.flatten()] = path[i+1,id1.flatten()]-1
         path[i,id2.flatten()] = path[i+1,id2.flatten()]+1
 
-    return (scores,path)
+    return path
 
 def test_seams():
     im = np.zeros((500, 500)).astype(float)
     im[10:25, 10:25] = 1.
 
-    scores,path = compute_scores(im)
-    import matplotlib.pyplot as plt
-    plt.subplot(1,2,1)
+    scores,direction = compute_scores(im)
+    path = backtrack(scores,direction)
+    ax1 = plt.subplot(1,2,1)
     plt.imshow(im,interpolation='nearest')
-    plt.subplot(1,2,2)
+    ax2 = plt.subplot(1,2,2)
     plt.imshow(scores,interpolation='nearest')
-    plt.colorbar()
-    plt.show()
+
+    #compute best path
+    idx = np.argsort(scores[-1,:])
+    ax1.plot(path[:,idx[-1]],np.arange(0,path.shape[0]),'k')
+    ax2.plot(path[:,idx[-1]],np.arange(0,path.shape[0]),'w')
     print path
+    plt.show()
+
 
 def test_fascicule():
-    import matplotlib.pyplot as plt
 
-    im = plt.imread('fascicule.jpg')[:,:,0].T
+    from skimage.filter.rank import median
+    from skimage.morphology import disk
 
-    scores,path = compute_scores(im)
+    im = plt.imread('fascicule.jpg')[:,:,0]
+    im = median(im,disk(3))
+
+    im = im.T
+    # im = im.T[-1::-1,:]
+
+    (scores,direction) = compute_scores(im)
+    path = backtrack(scores,direction)
 
     ax1 = plt.subplot(1,2,1)
     plt.imshow(im,interpolation='nearest')
     ax2 = plt.subplot(1,2,2)
-    plt.imshow(path,interpolation='nearest')
-    plt.colorbar()
-
-    print path
+    plt.imshow(scores,interpolation='nearest')
 
     #compute best path
-    x = np.argmax(scores[-1,:])
-    print x
-    p = [(x,path.shape[0]-1)]
-    for y in range(path.shape[0]-1,1,-1):
-        x = path[y,x]-1
-        p.append((x,y))
-    p = np.asarray(p)
-    ax2.plot(p[:,0],p[:,1])
+    idx = np.argsort(scores[-1,:])
+    ax1.plot(path[:,idx[-1]],np.arange(0,path.shape[0]),'k')
+    ax2.plot(path[:,idx[-1]],np.arange(0,path.shape[0]),'w')
+
+
     plt.show()
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
 
     test_fascicule()
+    # test_seams()
